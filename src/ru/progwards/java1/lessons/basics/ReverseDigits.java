84aff3f66f823fdf5031877f1b01f86ef09084b2
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2022-08-18 14:50:36 +0200

    1

---
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -0,0 +1,20 @@
+package ru.progwards.java1.lessons.basics;
+
+public class ReverseDigits {
+    public static void main(String[] args) {
+        System.out.println(reverseDigits(689));
+    }
+
+    public static int reverseDigits(int number) {
+        int n1;
+        n1 = number % 10;
+        number /= 10;
+        int n2 = number % 10;
+        number /= 10;
+        int n3 = number % 10;
+        int n4 = n1;
+        int n5 = n4 * 10 + n2;
+        return     n5 * 10 + n3;
+    }
+}
+
