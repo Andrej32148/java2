84305df7d913f053395f35fc0e16e290a44b20b5
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2022-09-07 19:45:23 +0200

    1

--- src/ru/progwards/java1/lessons/compare/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare/TriangleSimpleInfo.java
@@ -1,45 +1,48 @@
 package ru.progwards.java1.lessons.compare;
 
 public class TriangleSimpleInfo {
-    public static int maxSide(int a, int b, int c){
-        if (a>b&&a>c){
+    public static int maxSide(int a, int b, int c) {
+        if (a > b && a > c) {
             return a;
         }
-        if (b>a&&b>c){
+        if (b > a && b > c) {
             return b;
         }
-        if (c>b&&c>a){
+        if (c > b && c > a) {
             return c;
         }
         return 0;
-
     }
-    public static int minSide(int a, int b, int c){
-        if (a<b&&a<c){
+
+    public static int minSide(int a, int b, int c) {
+        if (a < b && a < c) {
             return a;
         }
-        if (b<a&&b<c){
+        if (b < a && b < c) {
             return b;
         }
 
-        if (c<b&&c<a){
-            return c;        }
+        if (c < b && c < a) {
+            return c;
+        }
 
 
         return 0;
 
     }
-    public static boolean isEquilateralTriangle(int a, int b, int c){
-        if (a==b&&b==c){
+
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        if (a == b && b == c) {
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(maxSide(5,1,12));
-        System.out.println(minSide(7,9,5));
+        System.out.println(maxSide(5, 1, 12));
+        System.out.println(minSide(7, 9, 5));
 
-    }}
+    }
+}
 
 
