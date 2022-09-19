6edd3ed74f8b2f1ec9d493df82ca44de7e043937
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2022-09-19 15:19:30 +0200

    1

--- src/ru/progwards/java1/lessons/cycles/DigitCheck.java
+++ src/ru/progwards/java1/lessons/cycles/DigitCheck.java
@@ -1,18 +1,19 @@
 package ru.progwards.java1.lessons.cycles;
 
 public class DigitCheck {
-    public static boolean containsDigit(int number, int digit){
-String s=String.valueOf(digit);
-String a=String.valueOf(number);
-        for (int i = 0; i <s.length(); i++) {
-            if(a.charAt(i)==s.charAt(0)) {
+        public static boolean containsDigit(int number, int digit) {
+            if (number == digit)
                 return true;
-            }
 
+            while (number != 0) {
+                if (number % 10 == digit)
+                    return true;
+                else
+                    number /= 10;
+            }
+            return false;
         }
-        return false;
-}
 
     public static void main(String[] args) {
-        System.out.println(containsDigit(589,5));
+        System.out.println(containsDigit(12345,5));
     }}
