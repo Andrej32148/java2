2e49682c70960f820369c83b371da52b6a8c95b8
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2022-10-09 17:49:07 +0200

    1

---
+++ src/ru/progwards/java1/lessons/useclasses/ComplexNum.java
@@ -0,0 +1,47 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class ComplexNum {
+
+    private final int i = (int) Math.sqrt(-1.0);
+    private int a;
+    private int b;
+
+
+    public ComplexNum(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public String toString() {
+        return a + "+" + b + "i";
+    }
+
+    public ComplexNum add(ComplexNum num) {
+
+        return new ComplexNum((a + num.a), (b + num.b));
+    }
+
+    public ComplexNum sub(ComplexNum num) {
+
+        return new ComplexNum((a - num.a), (b - num.b));
+    }
+
+    public ComplexNum mul(ComplexNum num) {
+
+        return new ComplexNum((a * num.a) - (b * num.b), (b * num.a) - (a * num.b));
+    }
+
+    public ComplexNum div(ComplexNum num) {
+        return new ComplexNum(((a * num.a) + (b * num.b)) / (num.a * num.a * num.b * num.b), ((b * num.a) - (a * num.b)) / (num.a * num.a * num.b * num.b));
+    }
+
+    public static void main(String[] args) {
+        ComplexNum c1 = new ComplexNum(2, 7);
+        ComplexNum c2 = new ComplexNum(3, 8);
+        System.out.println(c1.add(c2));
+        System.out.println(c1.mul(c2));
+        System.out.println(c1.sub(c2));
+
+
+    }
+}
---
+++ src/ru/progwards/java1/lessons/useclasses/Count.java
@@ -0,0 +1,37 @@
+package ru.progwards.java1.lessons.useclasses;
+
+public class Count {
+    int count ;
+
+    public Count(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void inc() {
+        count++;
+    }
+
+    public boolean dec() {
+        count--;
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+        Count count1 = new Count(10);
+        for (int i = count1.getCount(); i > 0; i--) {
+           boolean a= count1.dec();
+            System.out.println(count1.getCount());
+            if (a) {
+                System.out.println("count равен 0");
+                return;
+            }
+        }
+
+
+    }
+}
+
