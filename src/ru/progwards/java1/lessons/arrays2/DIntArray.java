fadc0ff4bfe9f08321d8a0ec4283f2b9997dd54f
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2023-01-08 15:57:14 +0200

    1

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -4,57 +4,55 @@ import java.util.Arrays;
 
 public class DIntArray {
     private int[] a;
-public DIntArray(){
 
-}
+    public DIntArray() {
+
+    }
+
     public DIntArray(int[] a) {
         this.a = a;
     }
 
+
+
+
     public void add(int num) {
-        int[] b = new int[a.length + 1];
-        for (int i = 0; i < a.length-1; i++) {
-            b[i] = a[i];
-        }
-        b[b.length-1] = num;
-        a = b;
+        int[] p = new int[a.length + 1];
+        System.arraycopy(a, 0, p, 0, a.length);
+        p[p.length - 1] = num;
+        a = p;
     }
 
     public void atInsert(int pos, int num) {
         int p[] = new int[a.length + 1];
-        for (int i = 0; i < pos; i++) {
-            p[i] = a[i];
-        }
-        for (int i = pos; i < p.length; i++) {
-            p[i] = a[i-1];
-        }
+        System.arraycopy(a, 0, p, 0, a.length);
+        p[pos] = num;
+        System.arraycopy(a, pos, p, pos + 1, a.length);
         a = p;
-        a[pos] = num;
     }
+
+
 
     public void atDelete(int pos) {
         int p[] = new int[a.length - 1];
-        for (int i = 0; i < pos; i++) {
-            p[i] = a[i];
-        }
-        for (int i = pos; i < p.length; i++) {
-            p[i] = a[i + 1];
-        }
+        System.arraycopy(a, 0, p, 0, a.length);
+        System.arraycopy(a, pos, p, pos - 1, a.length);
         a = p;
     }
-    public int at(int pos){
+
+    public int at(int pos) {
         return a[pos];
     }
 
 
-
     public static void main(String[] args) {
-        int[] b = {1, 6,8};
-        int[] a = {1, 6,8};
-DIntArray v=new DIntArray(a);
-v.atInsert(2,2);
-v.add(4);
-        System.out.println(Arrays.toString(a));
+        int[] b = {1, 6, 8};
+        int[] a = {1, 6, 8};
+        DIntArray v = new DIntArray(a);
+
+
+        v.atDelete(0);
+        System.out.println(Arrays.toString(v.a));
     }
 }
 
