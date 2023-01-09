f6e79f0ce13a987a52cb820d51d65de4bed26242
Andrej32148 <110244693+Andrej32148@users.noreply.github.com>
2023-01-09 20:27:53 +0200

    1

--- src/ru/progwards/java1/lessons/arrays2/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays2/DIntArray.java
@@ -1,58 +1,59 @@
 package ru.progwards.java1.lessons.arrays2;
 
-import java.util.Arrays;
 
 public class DIntArray {
-    private int[] a;
+    private int[] nums;
 
     public DIntArray() {
-
     }
-
-    public DIntArray(int[] a) {
-        this.a = a;
-    }
-
-
-
 
     public void add(int num) {
-        int[] p = new int[a.length + 1];
-        System.arraycopy(a, 0, p, 0, a.length);
-        p[p.length - 1] = num;
-        a = p;
+        int[] numsNew;
+        if (nums == null) {
+            numsNew = new int[1];
+            numsNew[0] = num;
+        }
+        else {
+            numsNew = new int[nums.length + 1];
+            System.arraycopy(nums, 0, numsNew, 0, nums.length);
+            numsNew[nums.length] = num;
+        }
+        nums = numsNew;
     }
 
     public void atInsert(int pos, int num) {
-        int p[] = new int[a.length + 1];
-        System.arraycopy(a, 0, p, 0, a.length);
-        p[pos] = num;
-        System.arraycopy(a, pos, p, pos + 1, a.length);
-        a = p;
+        int[] numsNew;
+        if (nums == null) {
+            numsNew = new int[1];
+            numsNew[0] = num;
+        }
+        else if (nums.length - 1 < pos) {
+            numsNew = new int[nums.length + 1];
+            System.arraycopy(nums, 0, numsNew, 0, nums.length);
+            numsNew[nums.length] = num;
+        } else {
+            numsNew = new int[nums.length + 1];
+            System.arraycopy(nums, 0, numsNew, 0, pos);
+            numsNew[pos] = num;
+            System.arraycopy(nums, pos, numsNew, pos + 1, nums.length - pos);
+        }
+        nums = numsNew;
     }
 
-
-
     public void atDelete(int pos) {
-        int p[] = new int[a.length - 1];
-        System.arraycopy(a, 0, p, 0, a.length);
-        System.arraycopy(a, pos, p, pos - 1, a.length);
-        a = p;
+        int[] numsNew = new int[nums.length - 1];
+        if (nums.length - 1 < pos) {
+            System.arraycopy(nums, 0, numsNew, 0, nums.length - 1);
+        } else {
+            System.arraycopy(nums, 0, numsNew, 0, pos);
+            System.arraycopy(nums, pos + 1, numsNew, pos, nums.length - pos - 1);
+        }
+        nums = numsNew;
     }
 
     public int at(int pos) {
-        return a[pos];
-    }
-
-
-    public static void main(String[] args) {
-        int[] b = {1, 6, 8};
-        int[] a = {1, 6, 8};
-        DIntArray v = new DIntArray(a);
-
-
-        v.atDelete(0);
-        System.out.println(Arrays.toString(v.a));
-    }
-}
-
+        if (nums.length - 1 < pos) {
+            return Integer.MIN_VALUE;
+        }
+        return nums[pos];
+    }}
